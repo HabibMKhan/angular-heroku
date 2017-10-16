fc4894ea34e7ad46f3c7f0233f2a3f748b343632
Habib Khan <habibmkhan92@gmail.com>
2017-10-16 15:36:04 -0700

    testing streamlabs token

--- app/welcome/welcome.js
+++ app/welcome/welcome.js
@@ -17,7 +17,7 @@ angular.module('webApp.welcome', ['ngRoute', 'firebase'])
 	}
 
 	var ref = firebase.database().ref().child('Articles');
-	$scope.articles = $firebaseArray(ref);	
+	$scope.articles = $firebaseArray(ref);
 
 	$scope.editPost = function(id){
 		var ref = firebase.database().ref().child('Articles/' + id);
@@ -49,5 +49,64 @@ angular.module('webApp.welcome', ['ngRoute', 'firebase'])
 
 	$scope.logout = function(){
 		CommonProp.logoutUser();
+	};
+
+	$scope.getToken = () => {
+		console.log('Yo it\'s Habib!');
+		const TWITCH_INFO = {
+			client_id: 'sdGTj9jKWwjI1mlfu9jZf2AFxzJS99HxL7PWNKad',
+			redirect_uri: 'https://angular-firebase-habib.herokuapp.com/#/welcome'
+		};
+		let tokenConfig = {
+			'client_id': TWITCH_INFO['client_id'],
+      'redirect_uri': TWITCH_INFO['redirect_uri'],
+      'response_type': 'code',
+      'scope': 'donations.read'
+		};
+
+		console.log('Just set up token config to contain: ', tokenConfig);
+		var data = JSON.stringify(false);
+
+		var xhr = new XMLHttpRequest();
+		xhr.withCredentials = true;
+
+		xhr.addEventListener("readystatechange", function () {
+		  if (this.readyState === this.DONE) {
+		    console.log(this.responseText);
+		  }
+		});
+		let qs = `response_type=${tokenConfig.response_type}&client_id=${tokenConfig.client_id}&redirect_uri=${tokenConfig.redirect_uri}&scope=${tokenConfig.scope}`
+
+		let url = "https://streamlabs.com/api/v1.0/authorize?" + qs;
+		console.log('url used', url);
+		xhr.open("GET", url );
+
+		xhr.send(data);
+		console.log('data received', data);
 	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }])
