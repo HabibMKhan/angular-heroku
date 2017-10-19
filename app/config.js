c468afd61d0f379d518289d6612c74623219a3e4
Habib Khan <habibmkhan92@gmail.com>
2017-10-18 17:55:17 -0700

    add config to heroku

---
+++ app/config.js
@@ -0,0 +1,10 @@
+const appConfig = {
+  STREAMLABS: {
+    grant_type: 'authorization_code',
+    client_id: 'sdGTj9jKWwjI1mlfu9jZf2AFxzJS99HxL7PWNKad',
+    client_secret: 'IMYTNlX6zg4l1q0Ky8hPhPvbUvLMnr53o3hA5isY',
+    redirect_uri: 'http://localhost:8000'
+  }
+}
+
+// module.exports = appConfig;
