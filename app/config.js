e24b6e43170fb39f52d9d3fc2d4a4f4880464bcb
Habib Khan <habibmkhan92@gmail.com>
2017-11-12 19:08:58 -0800

    point redirect uri to heroku

--- app/config.js
+++ app/config.js
@@ -3,7 +3,7 @@ const appConfig = {
     grant_type: 'authorization_code',
     client_id: 'sdGTj9jKWwjI1mlfu9jZf2AFxzJS99HxL7PWNKad',
     client_secret: 'IMYTNlX6zg4l1q0Ky8hPhPvbUvLMnr53o3hA5isY',
-    redirect_uri: 'http://localhost:8000'
+    redirect_uri: 'https://angular-firebase-habib.herokuapp.com'
   }
 }
 const firebaseConfig = {
